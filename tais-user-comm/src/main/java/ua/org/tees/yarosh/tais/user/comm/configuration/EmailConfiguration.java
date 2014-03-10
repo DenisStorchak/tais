17e88f9f70e908589f436c48bb11bea24fab779f
Timur Yarosh <ti.yarosh@gmail.com>
2014-03-10 17:45:08 +0200

    MailSender configuration

--- tais-user-comm/src/main/java/ua/org/tees/yarosh/tais/user/comm/configuration/EmailConfiguration.java
+++ tais-user-comm/src/main/java/ua/org/tees/yarosh/tais/user/comm/configuration/EmailConfiguration.java
@@ -1,5 +1,6 @@
 package ua.org.tees.yarosh.tais.user.comm.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
@@ -12,16 +13,31 @@ import java.util.Properties;
  */
 @Configuration
 public class EmailConfiguration {
+    private static final String MAIL_SMTP_AUTH_JMAIL_PROPERTY_NAME = "mail.smtp.auth";
+    private static final String MAIL_SMTP_SSL_ENABLE_JMAIL_PROPERTY_NAME = "mail.smtp.ssl.enable";
+    @Value("${host:smtp.yandex.ru}")
+    private String host;
+    @Value("${port:465}")
+    private int port;
+    @Value("${username:no-reply@tees.org.ua}")
+    private String username;
+    @Value("${password:SDG6dsASGvd}")
+    private String password;
+    @Value("${mail.smtp.auth:true}")
+    private String mailSmtpAuth;
+    @Value("${mail.smtp.ssl.enable:true}")
+    private String mailSmtpSslEnable;
+
     public JavaMailSenderImpl mailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setJavaMailProperties(new Properties());
 
-        mailSender.setHost("smtp.yandex.ru");
-        mailSender.setPort(465);
-        mailSender.setUsername("no-reply@tees.org.ua");
-        mailSender.setPassword("SDG6dsASGvd");
-        mailSender.getJavaMailProperties().setProperty("mail.smtp.auth", "true");
-        mailSender.getJavaMailProperties().setProperty("mail.smtp.ssl.enable", "true");
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+        mailSender.getJavaMailProperties().setProperty(MAIL_SMTP_AUTH_JMAIL_PROPERTY_NAME, mailSmtpAuth);
+        mailSender.getJavaMailProperties().setProperty(MAIL_SMTP_SSL_ENABLE_JMAIL_PROPERTY_NAME, mailSmtpSslEnable);
         return mailSender;
     }
 }
