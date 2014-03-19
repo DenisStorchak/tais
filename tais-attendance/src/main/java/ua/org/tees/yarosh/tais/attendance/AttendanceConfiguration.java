250e8b0310e93f33837811ff96e9684aa58343d6
Timur Yarosh <timur.jarosh@privatbank.ua>
2014-03-19 16:02:52 +0200

    1. spring-webmvc added to attendance as dependency 2. include attendance module to configuration

--- tais-attendance/src/main/java/ua/org/tees/yarosh/tais/attendance/AttendanceConfiguration.java
+++ tais-attendance/src/main/java/ua/org/tees/yarosh/tais/attendance/AttendanceConfiguration.java
@@ -3,9 +3,11 @@ package ua.org.tees.yarosh.tais.attendance;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(basePackageClasses = AttendanceConfiguration.class)
 @EnableJpaRepositories(basePackages = "ua.org.tees.yarosh.tais.attendance.schedule.api")
+@EnableWebMvc
 public class AttendanceConfiguration {
 }
--- tais-ui/src/main/java/ua/org/tees/yarosh/tais/ui/configuration/TaisConfiguration.java
+++ tais-ui/src/main/java/ua/org/tees/yarosh/tais/ui/configuration/TaisConfiguration.java
@@ -2,10 +2,11 @@ package ua.org.tees.yarosh.tais.ui.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import ua.org.tees.yarosh.tais.attendance.AttendanceConfiguration;
 import ua.org.tees.yarosh.tais.core.user.mgmt.configuration.UserMgmtConfiguration;
 import ua.org.tees.yarosh.tais.user.comm.configuration.UserCommConfiguration;
 
 @Configuration
-@Import({UserMgmtConfiguration.class, UserCommConfiguration.class})
+@Import({UserMgmtConfiguration.class, UserCommConfiguration.class, AttendanceConfiguration.class})
 public class TaisConfiguration {
 }
