3ec67a357117a68b6b4cf66dd388b67ec9426ddb
Timur Yarosh <timur.jarosh@privatbank.ua>
2014-03-26 16:26:50 +0200

    set vaadin version up to date, refactoring, vaadin theme compiling on compile phase, tests commented

--- tais-ui/src/main/java/ua/org/tees/yarosh/tais/ui/core/ApplicationServlet.java
+++ tais-ui/src/main/java/ua/org/tees/yarosh/tais/ui/core/ApplicationServlet.java
@@ -1,24 +1,27 @@
 package ua.org.tees.yarosh.tais.ui.core;
 
+import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.ServiceException;
 import com.vaadin.server.SessionInitEvent;
 import com.vaadin.server.SessionInitListener;
 import com.vaadin.server.VaadinServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ua.org.tees.yarosh.tais.ui.TAISUI;
 import ua.org.tees.yarosh.tais.ui.core.mvp.SpringManagedViewFactory;
 import ua.org.tees.yarosh.tais.ui.core.mvp.ViewFactory;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 
 import static org.springframework.web.context.support.WebApplicationContextUtils.getRequiredWebApplicationContext;
 import static ua.org.tees.yarosh.tais.ui.core.SessionKeys.VIEW_FACTORY;
 
-@WebServlet(urlPatterns = "/*", initParams = {
-        @WebInitParam(name = "UI", value = "ua.org.tees.yarosh.tais.ui.TAISUI")
-})
+//@WebServlet(urlPatterns = "/*", initParams = {
+//        @WebInitParam(name = "UI", value = "ua.org.tees.yarosh.tais.ui.TAISUI")
+//})
+@WebServlet(urlPatterns = "/*", asyncSupported = true)
+@VaadinServletConfiguration(productionMode = true, ui = TAISUI.class)
 public class ApplicationServlet extends VaadinServlet implements SessionInitListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationServlet.class);
