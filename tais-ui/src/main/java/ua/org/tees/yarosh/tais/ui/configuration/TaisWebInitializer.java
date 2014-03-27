1630646b85d0df0f16e6c7da662f7a70cf0edab1
Timur Yarosh <ti.yarosh@gmail.com>
2014-03-27 23:22:50 +0200

    proprietary api removed, ui build directory set to project root, source encoding set globally

--- tais-ui/src/main/java/ua/org/tees/yarosh/tais/ui/configuration/TaisWebInitializer.java
+++ tais-ui/src/main/java/ua/org/tees/yarosh/tais/ui/configuration/TaisWebInitializer.java
@@ -1,6 +1,5 @@
 package ua.org.tees.yarosh.tais.ui.configuration;
 
-import com.sun.org.apache.xerces.internal.parsers.SecurityConfiguration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -15,7 +14,6 @@ public class TaisWebInitializer implements WebApplicationInitializer {
     public void onStartup(ServletContext servletContext) throws ServletException {
         AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
         ctx.register(TaisConfiguration.class);
-        ctx.register(SecurityConfiguration.class);
         servletContext.addListener(new ContextLoaderListener(ctx));
 
         ctx.setServletContext(servletContext);
