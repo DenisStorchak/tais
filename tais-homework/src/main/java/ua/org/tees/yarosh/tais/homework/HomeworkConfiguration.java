7e3c567c6ab4bb36568ba29eb261ba6e4da9fbaf
Timur Yarosh <timur.jarosh@privatbank.ua>
2014-04-15 15:23:10 +0300

    amqp integration

--- tais-homework/src/main/java/ua/org/tees/yarosh/tais/homework/HomeworkConfiguration.java
+++ tais-homework/src/main/java/ua/org/tees/yarosh/tais/homework/HomeworkConfiguration.java
@@ -1,9 +1,7 @@
 package ua.org.tees.yarosh.tais.homework;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Import;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.context.annotation.*;
 import ua.org.tees.yarosh.tais.core.common.CommonConfiguration;
 import ua.org.tees.yarosh.tais.core.user.mgmt.UserMgmtConfiguration;
 
@@ -12,4 +10,13 @@ import ua.org.tees.yarosh.tais.core.user.mgmt.UserMgmtConfiguration;
 @ComponentScan(basePackageClasses = HomeworkConfiguration.class)
 @EnableAspectJAutoProxy
 public class HomeworkConfiguration {
+
+    @Bean
+    public CachingConnectionFactory connectionFactory() {
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory("localhost");
+        cachingConnectionFactory.setUsername("username");
+        cachingConnectionFactory.setPassword("password");
+        cachingConnectionFactory.setPort(5672);
+        return cachingConnectionFactory;
+    }
 }
