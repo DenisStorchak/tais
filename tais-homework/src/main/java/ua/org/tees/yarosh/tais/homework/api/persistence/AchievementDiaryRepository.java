2dacf1c2d6745cb16ed86fcdac4d3e0cf51a10a9
Timur Yarosh <timur.jarosh@privatbank.ua>
2014-04-18 09:58:57 +0300

    unnecessary annotations removed

--- tais-homework/src/main/java/ua/org/tees/yarosh/tais/homework/api/persistence/AchievementDiaryRepository.java
+++ tais-homework/src/main/java/ua/org/tees/yarosh/tais/homework/api/persistence/AchievementDiaryRepository.java
@@ -3,11 +3,9 @@ package ua.org.tees.yarosh.tais.homework.api.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import ua.org.tees.yarosh.tais.core.common.models.Registrant;
 import ua.org.tees.yarosh.tais.homework.models.AchievementDiary;
 
-@Repository
 public interface AchievementDiaryRepository extends JpaRepository<AchievementDiary, Long> {
     @Query("select a from AchievementDiary a where a.owner = :owner")
     AchievementDiary findOne(@Param("owner") Registrant owner);
--- tais-homework/src/main/java/ua/org/tees/yarosh/tais/homework/api/persistence/PersonalTaskHolderRepository.java
+++ tais-homework/src/main/java/ua/org/tees/yarosh/tais/homework/api/persistence/PersonalTaskHolderRepository.java
@@ -3,11 +3,9 @@ package ua.org.tees.yarosh.tais.homework.api.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import ua.org.tees.yarosh.tais.core.common.models.Registrant;
 import ua.org.tees.yarosh.tais.homework.models.PersonalTaskHolder;
 
-@Repository
 public interface PersonalTaskHolderRepository extends JpaRepository<PersonalTaskHolder, Long> {
     @Query("select p from PersonalTaskHolder p where p.owner = :registrant")
     PersonalTaskHolder findOne(@Param("registrant") Registrant registrant);
