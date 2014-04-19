07f7ff2007e3cbdb66caa60479b9b432e4b4b6a8
Timur Yarosh <ti.yarosh@gmail.com>
2014-04-19 22:01:50 +0300

    unnecessary annotation removed

--- tais-homework/src/main/java/ua/org/tees/yarosh/tais/homework/api/persistence/ManualTaskReportRepository.java
+++ tais-homework/src/main/java/ua/org/tees/yarosh/tais/homework/api/persistence/ManualTaskReportRepository.java
@@ -3,7 +3,6 @@ package ua.org.tees.yarosh.tais.homework.api.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import ua.org.tees.yarosh.tais.core.common.models.Discipline;
 import ua.org.tees.yarosh.tais.core.common.models.Registrant;
 import ua.org.tees.yarosh.tais.homework.models.ManualTask;
@@ -11,7 +10,6 @@ import ua.org.tees.yarosh.tais.homework.models.ManualTaskReport;
 
 import java.util.List;
 
-@Repository
 public interface ManualTaskReportRepository extends JpaRepository<ManualTaskReport, Long> {
     @Query("select m from ManualTaskReport m where m.task = :manualTask and m.owner = :registrant")
     ManualTaskReport findOne(@Param("manualTask") ManualTask manualTask, @Param("registrant") Registrant registrant);
