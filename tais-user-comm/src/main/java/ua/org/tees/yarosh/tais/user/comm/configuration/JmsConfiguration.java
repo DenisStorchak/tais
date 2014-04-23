ccce1045a201bdbe572edbcc58971bd05b67a6b2
Timur Yarosh <timur.jarosh@privatbank.ua>
2014-04-23 15:10:25 +0300

    jms configuration

---
+++ tais-user-comm/src/main/java/ua/org/tees/yarosh/tais/user/comm/configuration/JmsConfiguration.java
@@ -0,0 +1,31 @@
+package ua.org.tees.yarosh.tais.user.comm.configuration;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.pool.PooledConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.core.JmsTemplate;
+
+@Configuration
+public class JmsConfiguration {
+    @Bean
+    public ActiveMQConnectionFactory activeMQConnectionFactory() {
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+        connectionFactory.setBrokerURL("tcp://localhost:61616"); //todo move to jndi config
+        connectionFactory.setUserName("admin");
+        connectionFactory.setPassword("admin");
+        return connectionFactory;
+    }
+
+    @Bean
+    public PooledConnectionFactory jmsFactory() {
+        PooledConnectionFactory connectionFactory = new PooledConnectionFactory();
+        connectionFactory.setConnectionFactory(activeMQConnectionFactory());
+        return connectionFactory;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate() {
+        return new JmsTemplate(jmsFactory());
+    }
+}
